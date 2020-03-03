07534a087dee08c7414034dd2a871558a815efca
DhruvGo <61725476+DhruvGo@users.noreply.github.com>
2020-03-03 10:27:48 +0400

    Add files via upload

---
+++ Aircraft.java
@@ -0,0 +1,41 @@
+package week3;
+
+public class Aircraft {
+
+	public String callSign ;
+	public String regNO ;
+	public String type;
+	private int transpoderCode;
+	private String pilotName;
+	private String destination;
+	
+	public Aircraft(int c, int r, int t) {
+		this.callSign = "c";
+		this.regNO = "r";
+		this.type = "t";
+	}
+	public int gettranspoderCode() {
+		return this.transpoderCode;
+}
+	public void settranspoderCode(int s) {
+		this.transpoderCode = s;
+	}
+	public String getpilotName() {
+		return this.pilotName;
+	}
+	public void setpilotName(String p ) {
+		 this.pilotName = p;
+			}
+	public String setdestination() {
+		return this.destination;
+	}
+	public void getdestination(String d) {
+		this.destination =d;
+	}
+	public void printDetails() {
+		return;
+			}
+	public void Aircraft() {
+		return;
+	}
+}
