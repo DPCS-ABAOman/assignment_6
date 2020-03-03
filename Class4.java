473a2d0adfb6b47f18fc72553f17ee2e5002cb05
EmiliaJanusz <61725376+EmiliaJanusz@users.noreply.github.com>
2020-03-03 10:28:39 +0400

    Add files via upload

---
+++ Class4.java
@@ -0,0 +1,24 @@
+package myTest;
+import java.util.Arrays;
+import java.util.Scanner; 
+public class Class4 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// main method
+	
+	 Aircraft[] obj = new Aircraft [10];
+ 
+		int i=1;  
+	    while(i<=10){  
+	        System.out.println(i);  
+	    i++;  
+	
+	    }
+ 
+	Scanner input = new Scanner(System.in); 
+	   System.out.println("This is the scanner input"); 
+	    int num = input.nextLine();
+	
+	
+	}
+}
