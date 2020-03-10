b41540ed86eb56f83f69f319396bea8f50e18b6b
EmiliaJanusz <61725376+EmiliaJanusz@users.noreply.github.com>
2020-03-10 09:18:38 +0400

    Add files via upload

--- Aircraft.java
+++ Aircraft.java
@@ -1,41 +1,45 @@
-package week3;
+package myTest;
 
 public class Aircraft {
-
-	public String callSign ;
-	public String regNO ;
-	public String type;
-	private int transpoderCode;
-	private String pilotName;
-	private String destination;
+	// TODO Auto-generated method stub
+			// main method
+	public int callSign;
+	public int regNo;
+	public String type = "430N"; // make an array to put all "types"
 	
-	public Aircraft(int c, int r, int t) {
-		this.callSign = "c";
-		this.regNO = "r";
-		this.type = "t";
+	
+	private int Code; 
+	private String pilotName; 
+	private String destination; 
+	
+	public Aircraft (int c, int r, String t) {
+	this.callSign = c; 
+	this.regNo = r;
+	this.type = t;
+	
 	}
-	public int gettranspoderCode() {
-		return this.transpoderCode;
-}
-	public void settranspoderCode(int s) {
-		this.transpoderCode = s;
+		
+	public int getCode() { 
+		return this.Code; 
 	}
+
+	public void setCode(int C) { 
+		this.Code = C; 
+	}
+
 	public String getpilotName() {
 		return this.pilotName;
 	}
-	public void setpilotName(String p ) {
-		 this.pilotName = p;
-			}
-	public String setdestination() {
+	
+	public void setpilotName(String N) {
+		this.pilotName = N;
+	}
+	
+	public String getdestination() {
 		return this.destination;
 	}
-	public void getdestination(String d) {
-		this.destination =d;
-	}
-	public void printDetails() {
-		System.out.println();
-			}
-	public void Aircraft() {
-		System.out.println();
-	}
+	
+	
+	
+	
 }
--- Class4.java
+++ Class4.java
@@ -7,7 +7,32 @@ public class Class4 {
 		// main method
 	
 	 Aircraft[] obj = new Aircraft [10];
- 
+		 
+	 Scanner in = new Scanner(System.in);
+		 
+		int x;
+		int y;
+		int z;
+		 
+	for (int i = 0; i < obj.length; i++);{
+		System.out.println("enter the ..... /n");
+			x = in.nextInt();
+		System.out.println("enter the ..... /n");
+			y = in.nextInt();
+		System.out.println("enter the ..... /n");
+			z = in.nextInt();
+			
+		System.out.println("Sucessfully filled in elements" + i);
+			
+		obj[i] = new Aircraft (x, y, z);
+		}
+		
+		System.out.println("Finished");
+			in.close();
+		 
+	 
+	 
+	/* 
 		int i=1;  
 	    while(i<=10){  
 	        System.out.println(i);  
@@ -15,10 +40,17 @@ public class Class4 {
 	
 	    }
  
-	Scanner input = new Scanner(System.in); 
+	Scanner in = new Scanner(System.in); 
 	   System.out.println("This is the scanner input"); 
-	    int num = input.nextLine();
+	    String input = input.nextLine();
 	
-	
+	*/
+	    
+	//Part 2  
+	    		// for (obj = 0; Aircraft.length; i++);   
+	    		//  if(obj[i].getdestination > obj[i++].getdestination);
+	    		//    	max = i++;
+	    
+	    
 	}
 }
