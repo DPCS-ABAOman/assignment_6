dac700f18bafe6de4606ee0a2ba46a5a6fb51212
nn143 <57165323+nn143@users.noreply.github.com>
2020-04-05 14:14:19 +0400

    added comments from Mr Nik (Apr 5)

--- Kitchen.java
+++ Kitchen.java
@@ -2,13 +2,13 @@ package HRmanagement;
 
 public class Kitchen {
 
-	int ID;
+	int ID; // the id attribute would be a generic attribute for all employee types 
 	
-	String Status;
+	String Status; // what is the purpose of the Status? wouldn't it be evident by the class identifier? 
 	
 	int Hours;
 	
-	int Salary;
+	int Salary; // isn't the salary a unique quantity (that is hardcoded) for each type of employee? 
 	
 	public Kitchen(int ID, String s, int h, int sa) {
 		
